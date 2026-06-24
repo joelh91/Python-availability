328e762bd34a093dbd5d7c990a5163ff009680a9
joelhouapeu <joeloseelife@yahoo.com>
2026-06-24 00:29:37 -0400

    fix lint and unit test

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({
         "message": "Welcome to Joel's API"
     })
+
 
 @app.route("/health")
 def health():
@@ -14,5 +16,13 @@ def health():
         "status": "UP"
     })
 
+
+@app.route("/info")
+def info():
+    return jsonify({
+        "location": "Maryland"
+    })
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
--- test_cal.py
+++ test_cal.py
@@ -1,19 +1,28 @@
 from app import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"joel is learning"
+    assert response.get_json() == {
+        "message": "Welcome to Joel's API"
+    }
+
 
 def test_health():
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.data == b"its working fine"
+    assert response.get_json() == {
+        "status": "UP"
+    }
+
 
 def test_info():
     client = app.test_client()
     response = client.get("/info")
     assert response.status_code == 200
-    assert response.data == b"from Maryland"
+    assert response.get_json() == {
+        "location": "Maryland"
+    }
